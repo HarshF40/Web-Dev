831b7c5bac9535e881462bf25230457e0cf4bfef
HarshF40 <harshgaonkar57@gmail.com>
2025-02-04 00:56:09 +0530

    iterators and generators complete

--- MERN/JavaScript/Concepts/map.js
+++
@@ -1,8 +0,0 @@
-//returns an array
-//key value pair
-
-const number = [1,2,3,4,5];
-const squared = number.map((element) => {
-        return Math.pow(element, 2);
-});
-console.log(number, squared);
