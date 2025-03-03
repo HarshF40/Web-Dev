cb6b296e1b222d2bd30e65cc3b13168853407f81
HarshF40 <harshgaonkar57@gmail.com>
2025-03-03 08:58:28 +0530

    del Arch

--- MERN/Frontend/todo-app/src/components/TodoInput.jsx
+++ MERN/Frontend/todo-app/src/components/TodoInput.jsx
@@ -3,12 +3,15 @@ import { useState } from "react"
 export function TodoInput(props) {
 
   const { handleAddTodo } = props
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState('')
 
   return (
     <div className="input-container">
-      <input value={inputValue} placeholder="Add task" />
-      <button onclick={}><i class="fa-solid fa-plus"></i></button>
+      <input value={inputValue} onChange={(e) => {setInputValue(e.target.value)}} placeholder="Add task" />
+      <button onclick={() => {
+        if(!inputValue) { console.log("returning"); return }
+        handleAddTodo(inputValue)
+      }}><i class="fa-solid fa-plus"></i></button>
     </div>
   )
 }
